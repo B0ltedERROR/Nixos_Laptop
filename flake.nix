ad59bec0db629c692e4b4e58e113bdf662f38a20
James Davidson <JDavidson@xactiv.com>
2024-10-28 14:06:56 -0400

    removed home manager in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,17 +4,10 @@
     inputs = {
 	    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
         nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-        home-manager.url = "github:nix-community/home-manager/release-24.05";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-        firefox-addons = {
-            url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
 
     };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
 	let 
 	    lib = nixpkgs.lib;
 	    system = "x86_64-linux";
@@ -32,15 +25,5 @@
                 };
         };
 
-		homeConfigurations = {
-			gpskwlkr = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
-				modules = [ ./home ];
-                extraSpecialArgs = {
-                    inherit pkgs-unstable;
-                    inherit inputs;
-                };
-			};
-		};
     };
 }
